e128e89e33544da1d924eb81989c7d9e0e55fc83
RomanPeredrii <roman.peredrii@gmail.com>
2022-01-18 09:02:08 +0200

    recomit

--- routes/containers.js
+++ routes/containers.js
@@ -22,20 +22,20 @@ router.get('/', async (req, res) => {
 
 
 router.post('/edit', async (req, res) => {
-/******* get all containers here *******/    log('here container edit & containers page update', req.body)
-const {id} = req.body
-log('id = ', id)
-delete req.body.id
-log('body = ', req.body)
-try {
-    await Container.findOneAndUpdate(id, req.body)    
-
-} catch (error) {
-    log('EDIT CONTAINER ERROR', error)   
-}
-
-
+    /******* get all containers here *******/    log('here container edit & containers page update', req.body)
+    const {id} = req.body
+    log('id = ', id)
+    delete req.body.id
+    log('body = ', req.body)
+    try {
+        await Container.findOneAndUpdate(id, req.body)    
     
+    } catch (error) {
+        log('EDIT CONTAINER ERROR', error)   
+    }
+
+
+
 
 })
 
