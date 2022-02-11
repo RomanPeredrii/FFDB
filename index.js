969d7270e36ec81b1ec4a67d64978aa3a6dc0d9b
RomanPeredrii <roman.peredrii@gmail.com>
2022-02-11 13:29:03 +0200

    sessions is part working

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 const session = require("express-session")
 const path = require('path')
 const mongoose = require('mongoose')
+const User = require('./models/user')
 const addRoutes = require('./routes/add')
 const containersRoutes = require('./routes/containers')
 const homeRoutes = require('./routes/home')
@@ -15,7 +16,8 @@ const planningRoutes = require('./routes/planning')
 const authRoutes = require('./routes/auth')
 const {DB} = require('./data.js')
 const checkUser = require('./controllers/users')
-const { findById } = require('./models/container')
+const varMid = require('./middleware/variables')
+
 
 
 const hbs = expHBS.create({
@@ -29,20 +31,14 @@ const PORT = process.env.PORT || 3000
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
-app.use(express.static('public'))
 app.set('views', 'views')
 app.use(express.json({
     type: ['application/json', 'text/plain']
 }))
-app.use(session({
-    secret: "some secret",
-    resave: false, 
-    saveUninitialized: false
-}))
 
 app.use(async (req, res, next)=> {
     try {
-        const user = await findById('6205354ce210f5a486e4e1d8')
+        const user = await User.findById('6205354ce210f5a486e4e1d8')
         req.user = user
         next()        
     } catch (error) {
@@ -51,6 +47,15 @@ app.use(async (req, res, next)=> {
 
 })
 
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+app.use(session({
+    secret: "some secret",
+    resave: false, 
+    saveUninitialized: false
+}))
+
+app.use(varMid)
 app.use(express.urlencoded({extended: true}))
 app.use('/add', addRoutes)
 app.use('/containers', containersRoutes)
--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,19 @@ const {Router} = require('express')
 const router = Router()
 
 
-router.post('/', (req, res) => {
-    log('AUTH', req.body)
-    res.render('planning')
+router.post('/login', (req, res) => {
+    req.session.isAuthenticated = true
+    log('AUTH', req.session)
+    res.redirect('/containers')
 })
 
+router.get('/logout', (req, res) => {
+    req.session.destroy(()=> {
+        res.redirect('/')
+    })
+
+    
+})
 
 
 
