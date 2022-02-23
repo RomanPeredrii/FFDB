d23152245389b7199b918eaa3339e292519682a0
RomanPeredrii <roman.peredrii@gmail.com>
2022-02-23 23:31:01 +0200

    render planning

--- public/planning.js
+++ public/planning.js
@@ -21,8 +21,36 @@ log(data)
       },
       body: JSON.stringify(data),
     });
+    const containersPlanning = await response.json();
+    log( 'response', containersPlanning);
+    if (document.querySelector('#row')) {    
+        document.querySelectorAll('#row')
+        .forEach(node => document.querySelector('#makePlan')
+        .removeChild(node));
+    };
+log(document.querySelector('#makePlan'))
+    const row = document.querySelector('#makePlan');
+    containersPlanning.forEach(record => {
+    const tr = document.createElement('tr')
+    tr.setAttribute("id", "row");
+       log(tr)
+    row.appendChild(tr)
+    
+    .innerHTML=`<td class="number">${record.number}</td>
+    <td class="size">${record.size}</td>
+    <td class="status">${record.status}</td>
+    <td class="client">${record.client}</td>
+    <td class="POD">${record.POD}</td>
+    <td class="FD">${record.FD}</td>
+    <td class="BL">${record.BL}</td>
+    <td class="line">${record.line}</td>
+    <td class="vessel">${record.vessel}</td>          
+    <td class="driver">${record.driver}</td>
+    <td class="weight">${record.weight}</td>
+    <td class="cargo">${record.cargo}</td>
+    <td class="comments">${record.comments}</td>`
 
-    log(JSON.stringify(response))
-  });
+    })    
+});
 
 
